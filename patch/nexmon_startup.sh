806946ffcf2600bca4374082607e2cad844fcc9e
chatlxrdd <128418216+chatlxrdd@users.noreply.github.com>
2026-01-04 15:24:41 +0100

    Change Nexmon startup script to use absolute path
    Updated script to use absolute path instead of dynamic user variable.

--- patch/nexmon_startup.sh
+++ patch/nexmon_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo $USER
-# Move to your Nexmon directory
-cd /home/$USER/patch/nexmon || exit 1
+
+# IMPORTANT: Use absolute path instead of $USER
+cd /home/chatlxrd/NetStark/patch/nexmon || exit 1
 
 # Source environment
 source setup_env.sh
@@ -11,9 +11,9 @@ make
 
 # Move into the right patch directory
 cd patches/bcm43430a1/7_45_41_46/nexmon/ || exit 1
+
 # Build and install the patched firmware
 make
 make install-firmware
 
 exit 0
-
